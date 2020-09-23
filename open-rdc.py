8556e683c2fb27abdc6fe0479d0eb4642567f6e1
vinjn <vinjn.z@gmail.com>
2020-09-23 19:25:29 +0800

    Update

--- open-rdc.py
+++ open-rdc.py
@@ -1,5 +1,8 @@
 # https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe
 # https://renderdoc.org/docs/python_api/renderdoc/index.html
+# D:\svn_pool\renderdoc\renderdoc\replay\replay_controller.h
+# D:\svn_pool\renderdoc\renderdoc\api\replay\replay_enums.h
+
 import os
 import sys
 
@@ -8,7 +11,9 @@ os.environ["PATH"] += os.pathsep + os.path.abspath('../renderdoc/x64/Release')
 
 import renderdoc as rd
 
-rdc_file = 'D:/ue4-arpg.rdc'
+DUMP_RENDER_TARGET = True
+
+rdc_file = 'D:/rdc/YuanShen_2020.09.22_02.38.57_frame36700.rdc'
 
 def setup_rdc(filename):
     # Open a capture file handle
@@ -47,8 +52,28 @@ def iterDraw(d, level = 1):
     for d in d.children:
         iterDraw(d, level + 1)
 
+def dump_resource(controller, draw):
+    controller.SetFrameEvent(draw.eventId, True)
+    texsave = rd.TextureSave()
 
-def update_rdc(cap, controller):
+    if DUMP_RENDER_TARGET:
+        # dump render targtes (aka outputs)
+        for idx, output in enumerate(draw.outputs):
+            texsave.resourceId = output
+            if texsave.resourceId != rd.ResourceId.Null():
+                filename = "draw_%04d_c%d" % (draw.eventId, idx)
+                print("Saving images of %s at %d: %s" % (filename, draw.eventId, draw.name))
+                texsave.alpha = rd.AlphaMapping.BlendToCheckerboard
+                texsave.mip = 0
+                texsave.slice.sliceIndex = 0
+
+                # For formats with an alpha channel, preserve it
+                texsave.alpha = rd.AlphaMapping.Preserve
+
+                texsave.destType = rd.FileType.PNG
+                controller.SaveTexture(texsave, filename + ".png")
+
+def update_rdc(controller):
 
     file.write("* %s\n" % rdc_file)
     file.write("\n")
@@ -69,7 +94,7 @@ def update_rdc(cap, controller):
     inpass = False
 
     file.write("# Passes\n")
-    file.write("- Pass #0\n - starts with %d: %s\n" % (draw.eventId, draw.name))
+    file.write("- Pass #%d\n - starts with %d: %s\n" % (passnum, draw.eventId, draw.name))
 
     while draw != None:
         # When we encounter a clear
@@ -84,10 +109,35 @@ def update_rdc(cap, controller):
             passcontents += 1
             inpass = True
 
+        if False:
+            if draw.flags & rd.DrawFlags.Clear: file.write("Clear ")
+            if draw.flags & rd.DrawFlags.Drawcall: file.write("Drawcall ")
+            if draw.flags & rd.DrawFlags.Dispatch: file.write("Dispatch ")
+            if draw.flags & rd.DrawFlags.CmdList: file.write("CmdList ")
+            if draw.flags & rd.DrawFlags.SetMarker: file.write("SetMarker ")
+            if draw.flags & rd.DrawFlags.PushMarker: file.write("PushMarker ")
+            if draw.flags & rd.DrawFlags.PopMarker: file.write("PopMarker ")
+            if draw.flags & rd.DrawFlags.Present: file.write("Present ")
+            if draw.flags & rd.DrawFlags.MultiDraw: file.write("MultiDraw ")
+            if draw.flags & rd.DrawFlags.Copy: file.write("Copy ")
+            if draw.flags & rd.DrawFlags.Resolve: file.write("Resolve ")
+            if draw.flags & rd.DrawFlags.GenMips: file.write("GenMips ")
+            if draw.flags & rd.DrawFlags.PassBoundary: file.write("PassBoundary ")
+
+            if draw.flags & rd.DrawFlags.Indexed: file.write("Indexed ")
+            if draw.flags & rd.DrawFlags.Instanced: file.write("Instanced ")
+            if draw.flags & rd.DrawFlags.Auto: file.write("Auto ")
+            if draw.flags & rd.DrawFlags.Indirect: file.write("Indirect ")
+            if draw.flags & rd.DrawFlags.ClearColor: file.write("ClearColor ")
+            if draw.flags & rd.DrawFlags.ClearDepthStencil: file.write("ClearDepthStencil ")
+            if draw.flags & rd.DrawFlags.BeginPass: file.write("BeginPass ")
+            if draw.flags & rd.DrawFlags.EndPass: file.write("EndPass ")
+            if draw.flags & rd.DrawFlags.APICalls: file.write("APICalls ")
+
+        # dump_resource(controller, draw)
+
         # Advance to the next drawcall
         draw = draw.next
-        if draw is None:
-            break
 
     if inpass:
         file.write(" - contained %d draws\n" % (passcontents))
@@ -102,12 +152,18 @@ def shutdown_rdc(cap, controller):
     cap.Shutdown()
     rd.ShutdownReplay()
 
-output_name = rdc_file + ".report.md.html"
-file = open(output_name,"w") 
+if 'pyrenderdoc' in globals():
+    output_name = 'temp' + ".report.md.html"
+    file = open(output_name,"w") 
 
-cap, controller = setup_rdc(rdc_file)
-update_rdc(cap, controller)
-shutdown_rdc(cap, controller)
+    pyrenderdoc.Replay().BlockInvoke(update_rdc)
+else:
+    output_name = rdc_file + ".report.md.html"
+    file = open(output_name,"w") 
+
+    cap, controller = setup_rdc(rdc_file)
+    update_rdc(controller)
+    shutdown_rdc(cap, controller)
 
 markdeep_ending = """
 <meta charset="utf-8" emacsmode="-*- markdown -*-"><link rel="stylesheet" href="https://casual-effects.com/markdeep/latest/slate.css?">
